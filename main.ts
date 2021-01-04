70d3db9efc4f9714d7dbc493ce68d22e4f33776d
dora980315 <76759992+dora980315@users.noreply.github.com>
2021-01-04 12:10:40 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let projectile: Sprite = null
 scene.setBackgroundColor(9)
 let mySprite = sprites.create(img`
     ......cc66......
@@ -25,3 +26,22 @@ let mySprite = sprites.create(img`
     .....eeeeee.....
     .......ee.......
     `, SpriteKind.Player)
+game.onUpdateInterval(1000, function () {
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . 
+        e e e . . . . e e e . . . . 
+        c d d c . . c d d c . . . . 
+        c b d d f f d d b c . . . . 
+        c 3 b d d b d b 3 c . . . . 
+        f b 3 d d d d 3 b f . . . . 
+        e d d d d d d d d e . . . . 
+        e d f d d d d f d e b f b . 
+        f d d f d d f d d f f d f . 
+        f b d d b b d d 2 b f d f . 
+        . f 2 2 2 2 2 2 d b d b f . 
+        . f d d d d d d d f f f . . 
+        . f d b d f f f d f . . . . 
+        . . f f f f . . f f . . . . 
+        `, randint(-25, 25), randint(-25, 25))
+    null.lifespan = 3000
+})
